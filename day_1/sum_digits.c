75b4ca1d05c1b7b9e77c3c2cb469d0f46a3e3d89
Oye Oloyede <oyekunlemac@gmail.com>
2023-12-04 09:44:09 +0000

    First pass solution to day 1

--- day_1/sum_digits.c
+++ day_1/sum_digits.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #define MAX_LINE 1000
 
 int find_sum(char *input_file_name);
+int extract_digits(char *line);
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -11,9 +14,30 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    find_sum(argv[1]);
+    int sum_of_digits_in_file = find_sum(argv[1]);
+    printf("The sum of all the first and last digits in the file is %i\n", sum_of_digits_in_file);
 
     return EXIT_SUCCESS;
+}
+
+int extract_digits(char *line) {
+    int first_digit = -1;
+    int last_digit = -1;
+
+    for (size_t i = 0; i < strlen(line); i++) {
+        char current = line[i];
+        if (isdigit(current)) {
+            if (first_digit < 0) {
+                first_digit = current;
+            }
+
+            last_digit = current;
+        }
+    }
+
+    char first_and_last_digits[2] = {first_digit, last_digit};
+
+    return atoi(first_and_last_digits);
 }
 
 int find_sum(char *input_file_name) {
@@ -25,11 +49,13 @@ int find_sum(char *input_file_name) {
     }
 
     char buf[MAX_LINE];
+    int sum_of_digits = 0;
 
     while(fgets(buf, MAX_LINE, file_ptr) != NULL) {
-        printf("%s", buf);
+        sum_of_digits += extract_digits(buf);
     }
 
     fclose(file_ptr);
-    return 1;
+
+    return sum_of_digits;
 }
